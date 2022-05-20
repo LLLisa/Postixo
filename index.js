ace466c25dd13181ea95c4f234ee1749831b96f1
Lisa Knox <lisaknox83@gmail.com>
2022-05-20 11:31:16 -0400

    electron works, styling next

--- index.js
+++ index.js
@@ -21,8 +21,11 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
+  //style points
+  mainWindow.removeMenu(true);
+
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
